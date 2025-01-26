93862058c78a8dbce09d45a98bb3b1518a8256c7
juancrfig <juancrfig@gmail.com>
2025-01-26 18:50:42 -0500

    Create factory function for creating a player

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+function Game(playerOne, playerTwo) {
+
+}
+
 function Gameboard(score) {
     this.score = score;
 }
 
-function Player(name, score=0) {
-    this.name = name;
-    this.score = score;
+
+// This is a FACTORY FUNCTION
+function createPlayer(name, score=0) {
+    // Private Data
+
+    // Public Interface (returned object)
+    return {
+        name,
+        score
+    };
 }
